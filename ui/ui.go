579469c4047c90b34a04b4a15d4598342aadbb88
Benjamin Walkenhorst <krylon@posteo.de>
2022-06-01 00:20:02 +0200

    Beat the UI into some shape where we can actually display something

--- ui/ui.go
+++ ui/ui.go
@@ -2,14 +2,15 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 12. 09. 2021 by Benjamin Walkenhorst
 // (c) 2021 Benjamin Walkenhorst
-// Time-stamp: <2021-09-18 18:32:04 krylon>
+// Time-stamp: <2022-05-31 23:18:21 krylon>
 
 package ui
 
 import (
+	"fmt"
 	"log"
+	"time"
 
-	"github.com/blicero/krylib"
 	"github.com/blicero/raconteur/common"
 	"github.com/blicero/raconteur/db"
 	"github.com/blicero/raconteur/logdomain"
@@ -22,6 +23,7 @@ type RWin struct {
 	log       *log.Logger
 	win       *gtk.Window
 	mainBox   *gtk.Box
+	lbl       *gtk.Label
 	menu      *gtk.MenuBar
 	statusbar *gtk.Statusbar
 }
@@ -45,11 +47,43 @@ func Create() (*RWin, error) {
 		win.log.Printf("[ERROR] Cannot create main window: %s\n",
 			err.Error())
 		return nil, err
-	} else if win.mainBox, err = gtk.BoxNew(gtk.ORIENTATION_VERTICAL); err != nil {
+	} else if win.mainBox, err = gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 1); err != nil {
 		win.log.Printf("[ERROR] Cannot create main Box: %s\n",
 			err.Error())
 		return nil, err
+	} else if win.lbl, err = gtk.LabelNew("Wer das liest, ist doof"); err != nil {
+		win.log.Printf("[ERROR] Cannot create label: %s\n",
+			err.Error())
+		return nil, err
+	} else if win.statusbar, err = gtk.StatusbarNew(); err != nil {
+		win.log.Printf("[ERROR] Cannot create status bar: %s\n", err.Error())
+		return nil, err
 	}
 
-	return nil, krylib.ErrNotImplemented
+	win.win.Add(win.mainBox)
+	win.mainBox.Add(win.lbl)
+	win.mainBox.Add(win.statusbar)
+
+	win.win.Connect("destroy", gtk.MainQuit)
+
+	win.win.ShowAll()
+
+	return win, nil
 } // func Create() (*RWin, error)
+
+// Run execute's gtk's main event loop.
+func (w *RWin) Run() {
+	go func() {
+		var cnt = 0
+		for {
+			time.Sleep(time.Second)
+			cnt += 1
+			var msg = fmt.Sprintf("%s: Tick #%d",
+				time.Now().Format(common.TimestampFormat),
+				cnt)
+			w.statusbar.Push(666, msg)
+		}
+	}()
+
+	gtk.Main()
+} // func (w *RWin) Run()

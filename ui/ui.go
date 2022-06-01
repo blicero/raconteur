e043918fb73650c0248663634a39219517e9f0fb
Benjamin Walkenhorst <krylon@posteo.de>
2022-06-01 23:44:19 +0200

    More work on the GUI

--- ui/ui.go
+++ ui/ui.go
@@ -2,30 +2,99 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 12. 09. 2021 by Benjamin Walkenhorst
 // (c) 2021 Benjamin Walkenhorst
-// Time-stamp: <2022-05-31 23:18:21 krylon>
+// Time-stamp: <2022-06-01 23:43:11 krylon>
 
 package ui
 
 import (
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
+	"github.com/blicero/krylib"
 	"github.com/blicero/raconteur/common"
 	"github.com/blicero/raconteur/db"
 	"github.com/blicero/raconteur/logdomain"
+	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 )
 
+// type tabContent struct {
+// 	vbox   *gtk.Box
+// 	sbox   *gtk.Box
+// 	lbl    *gtk.Label
+// 	search *gtk.Entry
+// 	store  gtk.ITreeModel
+// 	view   *gtk.TreeView
+// 	scr    *gtk.ScrolledWindow
+// }
+
+// type cellEditHandlerFactory func(int) func(*gtk.CellRendererText, string, string)
+
+type column struct {
+	colType glib.Type
+	title   string
+	edit    bool
+}
+
+var cols = []column{
+	column{
+		colType: glib.TYPE_STRING,
+		title:   "Program",
+	},
+	column{
+		colType: glib.TYPE_STRING,
+		title:   "Title",
+		edit:    true,
+	},
+	column{
+		colType: glib.TYPE_INT,
+		title:   "#",
+		edit:    true,
+	},
+	column{
+		colType: glib.TYPE_STRING,
+		title:   "Dur",
+	},
+}
+
+func createCol(title string, id int) (*gtk.TreeViewColumn, *gtk.CellRendererText, error) {
+	krylib.Trace()
+	defer fmt.Printf("[TRACE] EXIT %s\n",
+		krylib.TraceInfo())
+
+	renderer, err := gtk.CellRendererTextNew()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	col, err := gtk.TreeViewColumnNewWithAttribute(title, renderer, "text", id)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	col.SetResizable(true)
+
+	return col, renderer, nil
+} // func createCol(title string, id int) (*gtk.TreeViewColumn, *gtk.CellRendererText, error)
+
 // RWin is the user-facing part of the whole shebang.
+// nolint: structcheck,unused
 type RWin struct {
-	pool      *db.Pool
-	log       *log.Logger
-	win       *gtk.Window
-	mainBox   *gtk.Box
-	lbl       *gtk.Label
-	menu      *gtk.MenuBar
-	statusbar *gtk.Statusbar
+	pool        *db.Pool
+	lock        sync.RWMutex
+	log         *log.Logger
+	win         *gtk.Window
+	mainBox     *gtk.Box
+	searchBox   *gtk.Box
+	searchLbl   *gtk.Label
+	searchEntry *gtk.Entry
+	store       gtk.ITreeModel
+	view        *gtk.TreeView
+	scr         *gtk.ScrolledWindow
+	menu        *gtk.MenuBar
+	statusbar   *gtk.Statusbar
 }
 
 // Create creates a GUI. Who would've thought?
@@ -51,17 +120,60 @@ func Create() (*RWin, error) {
 		win.log.Printf("[ERROR] Cannot create main Box: %s\n",
 			err.Error())
 		return nil, err
-	} else if win.lbl, err = gtk.LabelNew("Wer das liest, ist doof"); err != nil {
-		win.log.Printf("[ERROR] Cannot create label: %s\n",
+	} else if win.searchBox, err = gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 1); err != nil {
+		win.log.Printf("[ERROR] Cannot create searchBox: %s\n",
+			err.Error())
+		return nil, err
+	} else if win.searchLbl, err = gtk.LabelNew("Search: "); err != nil {
+		win.log.Printf("[ERROR] Cannot create search label: %s\n",
 			err.Error())
 		return nil, err
 	} else if win.statusbar, err = gtk.StatusbarNew(); err != nil {
 		win.log.Printf("[ERROR] Cannot create status bar: %s\n", err.Error())
 		return nil, err
+	} // else if win.store, err = gtk.TreeStoreNew(
+
+	var typeList = make([]glib.Type, len(cols))
+
+	for i, c := range cols {
+		typeList[i] = c.colType
 	}
 
+	if win.store, err = gtk.TreeStoreNew(typeList...); err != nil {
+		win.log.Printf("[ERROR] Cannot create TreeStore: %s\n",
+			err.Error())
+		return nil, err
+	} else if win.view, err = gtk.TreeViewNewWithModel(win.store); err != nil {
+		win.log.Printf("[ERROR] Cannot create TreeView: %s\n",
+			err.Error())
+		return nil, err
+	}
+
+	for i, c := range cols {
+		var (
+			col      *gtk.TreeViewColumn
+			renderer *gtk.CellRendererText
+		)
+
+		if col, renderer, err = createCol(c.title, i); err != nil {
+			win.log.Printf("[ERROR] Cannot create TreeViewColumn %q: %s\n",
+				c.title,
+				err.Error())
+			return nil, err
+		}
+
+		renderer.Set("editable", c.edit)     // nolint: errcheck
+		renderer.Set("editable-set", c.edit) // nolint: errcheck
+
+		win.view.AppendColumn(col)
+	}
+
+	win.searchBox.Add(win.searchLbl)
+	win.searchBox.Add(win.searchEntry)
 	win.win.Add(win.mainBox)
-	win.mainBox.Add(win.lbl)
+	win.scr.Add(win.view)
+	win.mainBox.Add(win.searchBox)
+	win.mainBox.Add(win.scr)
 	win.mainBox.Add(win.statusbar)
 
 	win.win.Connect("destroy", gtk.MainQuit)
@@ -77,7 +189,7 @@ func (w *RWin) Run() {
 		var cnt = 0
 		for {
 			time.Sleep(time.Second)
-			cnt += 1
+			cnt++
 			var msg = fmt.Sprintf("%s: Tick #%d",
 				time.Now().Format(common.TimestampFormat),
 				cnt)
@@ -87,3 +199,50 @@ func (w *RWin) Run() {
 
 	gtk.Main()
 } // func (w *RWin) Run()
+
+// nolint: unused
+func (w *RWin) displayMsg(msg string) {
+	krylib.Trace()
+	defer w.log.Printf("[TRACE] EXIT %s\n",
+		krylib.TraceInfo())
+
+	var (
+		err error
+		dlg *gtk.Dialog
+		lbl *gtk.Label
+		box *gtk.Box
+	)
+
+	if dlg, err = gtk.DialogNewWithButtons(
+		"Message",
+		w.win,
+		gtk.DIALOG_MODAL,
+		[]interface{}{
+			"Okay",
+			gtk.RESPONSE_OK,
+		},
+	); err != nil {
+		w.log.Printf("[ERROR] Cannot create dialog to display message: %s\nMesage would've been %q\n",
+			err.Error(),
+			msg)
+		return
+	}
+
+	defer dlg.Close()
+
+	if lbl, err = gtk.LabelNew(msg); err != nil {
+		w.log.Printf("[ERROR] Cannot create label to display message: %s\nMessage would've been: %q\n",
+			err.Error(),
+			msg)
+		return
+	} else if box, err = dlg.GetContentArea(); err != nil {
+		w.log.Printf("[ERROR] Cannot get ContentArea of Dialog to display message: %s\nMessage would've been %q\n",
+			err.Error(),
+			msg)
+		return
+	}
+
+	box.PackStart(lbl, true, true, 0)
+	dlg.ShowAll()
+	dlg.Run()
+} // func (g *GUI) displayMsg(msg string)

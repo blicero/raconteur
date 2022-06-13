ffeb8f0f7d22eef2b177e771cd4cfbc3faa70991
Benjamin Walkenhorst <krylon@posteo.de>
2022-06-13 21:27:30 +0200

    main: Print version number and build date upon start

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 30. 05. 2022 by Benjamin Walkenhorst
 // (c) 2022 Benjamin Walkenhorst
-// Time-stamp: <2022-05-31 20:18:53 krylon>
+// Time-stamp: <2022-06-13 18:58:12 krylon>
 
 package main
 
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/blicero/raconteur/common"
 	"github.com/blicero/raconteur/ui"
 )
 
@@ -18,6 +19,11 @@ func main() {
 		win *ui.RWin
 		err error
 	)
+
+	fmt.Printf("%s %s, built on %s starting up...\n",
+		common.AppName,
+		common.Version,
+		common.BuildStamp.Format(common.TimestampFormat))
 
 	if win, err = ui.Create(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error creating UI: %s\n", err.Error())

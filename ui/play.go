1c0097748ba359ff87ef09862527f6012fbcd1d4
Benjamin Walkenhorst <krylon@posteo.de>
2022-06-18 20:29:51 +0200

    Move stuff from mbus.go to play.go, delete mbus.go

--- ui/play.go
+++ ui/play.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 15. 06. 2022 by Benjamin Walkenhorst
 // (c) 2022 Benjamin Walkenhorst
-// Time-stamp: <2022-06-17 17:52:28 krylon>
+// Time-stamp: <2022-06-18 20:27:03 krylon>
 
 package ui
 
@@ -14,9 +14,46 @@ import (
 	"github.com/blicero/krylib"
 	"github.com/blicero/raconteur/db"
 	"github.com/blicero/raconteur/objects"
+	"github.com/godbus/dbus/v5"
 )
 
-const playerPath = "/usr/bin/smplayer"
+const (
+	playerPath     = "/usr/bin/vlc"
+	objName        = "org.mpris.MediaPlayer2.vlc"
+	objPath        = "/org/mpris/MediaPlayer2"
+	objInterface   = "org.mpris.MediaPlayer2.Player"
+	trackInterface = "org.mpris.MediaPlayer2.TrackList"
+	trackList      = "org.mpris.MediaPlayer2.TrackList.Tracks"
+	noTrack        = "/org/mpris/MediaPlayer2/TrackList/NoTrack"
+	addTrack       = "org.mpris.MediaPlayer2.TrackList.AddTrack"
+	delTrack       = "org.mpris.MediaPlayer2.TrackList.RemoveTrack"
+)
+
+func (w *RWin) getPlayerStatus() (string, error) {
+	var (
+		err error
+		str string
+		val dbus.Variant
+		obj = w.mbus.Object(objName, objPath)
+	)
+
+	if val, err = obj.GetProperty("org.mpris.MediaPlayer2.Player.PlaybackStatus"); err != nil {
+		w.log.Printf("[ERROR] Cannot get player status: %s\n",
+			err.Error())
+		return "", err
+	}
+
+	str = val.Value().(string)
+
+	w.log.Printf("[DEBUG] PlaybackStatus is %s\n",
+		str)
+
+	if str == "Playing" {
+		// get the file and position, save it
+	}
+
+	return str, nil
+} // func (w *RWin) getPlayerStatus() (string, error)
 
 func (w *RWin) playerCreate() error {
 	krylib.Trace()
@@ -29,8 +66,8 @@ func (w *RWin) playerCreate() error {
 	if !w.playerActive {
 		var cmd = exec.Command(
 			playerPath,
-			"-no-fullscreen",
-			"-no-close-at-end",
+			"--no-fullscreen",
+			// "-no-close-at-end",
 		)
 
 		if err := cmd.Start(); err != nil {
@@ -75,6 +112,7 @@ func (w *RWin) playerPlayProgram(p *objects.Program) {
 		err   error
 		c     *db.Database
 		files []objects.File
+		obj   = w.mbus.Object(objName, objPath)
 	)
 
 	c = w.pool.Get()
@@ -90,24 +128,28 @@ func (w *RWin) playerPlayProgram(p *objects.Program) {
 		return
 	}
 
-	var args = make([]string, len(files)+1)
+	for _, f := range files {
+		w.log.Printf("[TRACE] Add %s to Playlist\n",
+			f.DisplayTitle())
 
-	args[0] = "-add-to-playlist"
+		var res = obj.Call(
+			addTrack,
+			0, // dbus.FlagNoReplyExpected,
+			f.PathURL(),
+			dbus.ObjectPath(noTrack),
+			false,
+		)
 
-	for i, f := range files {
-		args[i+1] = f.Path
+		if res.Err != nil {
+			w.log.Printf("[ERROR] DBus method call failed: %s\n",
+				res.Err.Error())
+		}
 	}
 
-	var cmd = exec.Command(playerPath, args...)
-
-	if err = cmd.Start(); err != nil {
-		var msg = fmt.Sprintf("Failed to run %s: %s",
-			playerPath,
-			err.Error())
-		w.log.Println("[ERROR] " + msg)
-		w.displayMsg(msg)
-		return
-	}
+	obj.Call(
+		"org.mpris.MediaPlayer2.Player.Play",
+		dbus.FlagNoReplyExpected,
+	)
 } // func (w *RWin) playerPlayProgram(p *objects.Program)
 
 func (w *RWin) playerClearPlaylist() error {
@@ -115,17 +157,27 @@ func (w *RWin) playerClearPlaylist() error {
 	defer fmt.Printf("[TRACE] EXIT %s\n",
 		krylib.TraceInfo())
 
-	var err error
-	var proc = exec.Command(
-		playerPath,
-		"-send-action",
-		"pl_remove_all",
+	var (
+		err error
+		val dbus.Variant
+		obj = w.mbus.Object(objName, objPath)
 	)
 
-	if err = proc.Start(); err != nil {
-		w.log.Printf("[ERROR] Cannot run smplayer action pl_remove_all: %s\n",
+	if val, err = obj.GetProperty(trackList); err != nil {
+		w.log.Printf("[ERROR] Cannot get TrackList from Player: %s\n",
 			err.Error())
 		return err
+	}
+
+	var items = val.Value().([]dbus.ObjectPath)
+
+	for _, i := range items {
+		w.log.Printf("[TRACE] Process %s\n",
+			i)
+		obj.Call(
+			delTrack,
+			dbus.FlagNoReplyExpected,
+			i)
 	}
 
 	return nil

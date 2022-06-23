c6e474cafa31ffc9df2f606e33710cbd2b729506
Benjamin Walkenhorst <krylon@posteo.de>
2022-06-23 19:45:05 +0200

    UI: Get data about track played and position from Player via DBus

--- ui/play.go
+++ ui/play.go
@@ -2,16 +2,18 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 15. 06. 2022 by Benjamin Walkenhorst
 // (c) 2022 Benjamin Walkenhorst
-// Time-stamp: <2022-06-20 22:28:14 krylon>
+// Time-stamp: <2022-06-23 19:37:34 krylon>
 
 package ui
 
 import (
 	"fmt"
+	"net/url"
 	"os/exec"
 	"time"
 
 	"github.com/blicero/krylib"
+	"github.com/blicero/raconteur/common"
 	"github.com/blicero/raconteur/db"
 	"github.com/blicero/raconteur/objects"
 	"github.com/godbus/dbus/v5"
@@ -27,6 +29,9 @@ const (
 	noTrack        = "/org/mpris/MediaPlayer2/TrackList/NoTrack"
 	addTrack       = "org.mpris.MediaPlayer2.TrackList.AddTrack"
 	delTrack       = "org.mpris.MediaPlayer2.TrackList.RemoveTrack"
+	propStatus     = "org.mpris.MediaPlayer2.Player.PlaybackStatus"
+	propPosition   = "org.mpris.MediaPlayer2.Player.Position"
+	propMeta       = "org.mpris.MediaPlayer2.Player.Metadata"
 )
 
 func (w *RWin) getPlayerStatus() (string, error) {
@@ -37,7 +42,7 @@ func (w *RWin) getPlayerStatus() (string, error) {
 		obj = w.mbus.Object(objName, objPath)
 	)
 
-	if val, err = obj.GetProperty("org.mpris.MediaPlayer2.Player.PlaybackStatus"); err != nil {
+	if val, err = obj.GetProperty(propStatus); err != nil {
 		// w.log.Printf("[ERROR] Cannot get player status: %s\n",
 		// 	err.Error())
 		return "", err
@@ -48,8 +53,87 @@ func (w *RWin) getPlayerStatus() (string, error) {
 	w.log.Printf("[DEBUG] PlaybackStatus is %s\n",
 		str)
 
-	if str == "Playing" {
-		// get the file and position, save it
+	if str == "Playing" || str == "Paused" {
+		var (
+			meta map[string]dbus.Variant
+			pos  int64
+			ok   bool
+		)
+
+		if val, err = obj.GetProperty(propPosition); err != nil {
+			w.log.Printf("[ERROR] Cannot get Position: %s\n",
+				err.Error())
+			return "", err
+		} else if pos, ok = val.Value().(int64); !ok {
+			w.log.Printf("[ERROR] Cannot convert result to int64: %T\n",
+				val.Value())
+			return "", fmt.Errorf("Cannot convert result to int64: %T",
+				val.Value())
+		} else if val, err = obj.GetProperty(propMeta); err != nil {
+			w.log.Printf("[ERROR] Cannot get Property %s: %s\n",
+				propMeta,
+				err.Error())
+			return "", err
+		} else if meta, ok = val.Value().(map[string]dbus.Variant); !ok {
+			w.log.Printf("[ERROR] Wrong type for %s: %T\n",
+				propMeta,
+				val.Value())
+			return "", fmt.Errorf("Wrong type for %s: %T",
+				propMeta,
+				val.Value())
+		}
+
+		var sec = time.Microsecond * time.Duration(pos)
+
+		w.log.Printf("[DEBUG] Player is at position %s\n",
+			sec)
+
+		if common.Debug {
+			for k, v := range meta {
+				w.log.Printf("[DEBUG] Meta %-15s => (%T) %#v\n",
+					k,
+					v.Value(),
+					v.Value())
+			}
+		}
+
+		var (
+			uriRaw, uriEsc string
+			fileURL        *url.URL
+		)
+
+		uriRaw = meta["xesam:url"].Value().(string)
+
+		if uriEsc, err = url.PathUnescape(uriRaw); err != nil {
+			w.log.Printf("[ERROR] Cannot un-escape URL path %q: %s\n",
+				uriRaw,
+				err.Error())
+			return "", err
+		} else if fileURL, err = url.Parse(uriEsc); err != nil {
+			w.log.Printf("[ERROR] Cannot parse URL %q: %s\n",
+				uriEsc,
+				err.Error())
+			return "", err
+		} else if common.Debug {
+			w.log.Printf("[DEBUG] Currently playing %s at %s\n",
+				fileURL.Path,
+				sec)
+		}
+
+		var (
+			c *db.Database
+			f *objects.File
+		)
+
+		c = w.pool.Get()
+		defer w.pool.Put(c)
+
+		if f, err = c.FileGetByPath(fileURL.Path); err != nil {
+			w.log.Printf("[ERROR] Cannot look for File %s: %s\n",
+				fileURL.Path,
+				err.Error())
+			return "", err
+		}
 	}
 
 	return str, nil

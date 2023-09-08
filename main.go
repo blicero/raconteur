96b6095055ddcd23a1553a38b254105516a0a913
Benjamin Walkenhorst <krylon@posteo.de>
2023-09-09 00:57:26 +0200

    Add command line flag to clear out the log file

--- main.go
+++ main.go
@@ -2,11 +2,12 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 30. 05. 2022 by Benjamin Walkenhorst
 // (c) 2022 Benjamin Walkenhorst
-// Time-stamp: <2022-06-13 18:58:12 krylon>
+// Time-stamp: <2023-09-08 21:12:31 krylon>
 
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -16,14 +17,23 @@ import (
 
 func main() {
 	var (
-		win *ui.RWin
-		err error
+		err      error
+		win      *ui.RWin
+		clearLog bool
 	)
 
 	fmt.Printf("%s %s, built on %s starting up...\n",
 		common.AppName,
 		common.Version,
 		common.BuildStamp.Format(common.TimestampFormat))
+
+	flag.BoolVar(&clearLog, "clear", false, "Truncate the log file at startup")
+
+	flag.Parse()
+
+	if clearLog {
+		os.Remove(common.LogPath) // nolint: errcheck
+	}
 
 	if win, err = ui.Create(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error creating UI: %s\n", err.Error())

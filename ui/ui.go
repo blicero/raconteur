30d762b839116cd28969c625f3c8ab9760b7a636
Benjamin Walkenhorst <krylon@posteo.de>
2023-09-14 19:35:55 +0200

    Edit File Metadata

--- ui/ui.go
+++ ui/ui.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 12. 09. 2021 by Benjamin Walkenhorst
 // (c) 2021 Benjamin Walkenhorst
-// Time-stamp: <2023-09-13 20:35:08 krylon>
+// Time-stamp: <2023-09-14 19:31:49 krylon>
 
 package ui
 
@@ -1232,12 +1232,13 @@ func (w *RWin) fileSetProgram(iter *gtk.TreeIter, f *objects.File, p *objects.Pr
 
 func (w *RWin) editFile(f *objects.File, iter *gtk.TreeIter) {
 	var (
-		err           error
-		dlg           *gtk.Dialog
-		dbox          *gtk.Box
-		grid          *gtk.Grid
-		titleE, urlE  *gtk.Entry
-		trackE, discE *gtk.SpinButton
+		err                         error
+		dlg                         *gtk.Dialog
+		dbox                        *gtk.Box
+		grid                        *gtk.Grid
+		titleE, urlE                *gtk.Entry
+		trackE, discE               *gtk.SpinButton
+		titleL, urlL, trackL, discL *gtk.Label
 	)
 
 	if dlg, err = gtk.DialogNewWithButtons(
@@ -1259,6 +1260,160 @@ func (w *RWin) editFile(f *objects.File, iter *gtk.TreeIter) {
 
 	defer dlg.Close()
 
+	if _, err = dlg.AddButton("OK", gtk.RESPONSE_OK); err != nil {
+		w.log.Printf("[ERROR] Cannot add OK button to AddProgram Dialog: %s\n",
+			err.Error())
+		return
+	} else if grid, err = gtk.GridNew(); err != nil {
+		w.log.Printf("[ERROR] Cannot create gtk.Grid for AddPerson Dialog: %s\n",
+			err.Error())
+		return
+	} else if titleL, err = gtk.LabelNew("Title:"); err != nil {
+		w.log.Printf("[ERROR] Cannot create Title Label: %s\n",
+			err.Error())
+		return
+	} else if urlL, err = gtk.LabelNew("URL:"); err != nil {
+		w.log.Printf("[ERROR] Cannot create URL Label: %s\n",
+			err.Error())
+		return
+	} else if trackL, err = gtk.LabelNew("Track:"); err != nil {
+		w.log.Printf("[ERROR] Cannot create Track Label: %s\n",
+			err.Error())
+		return
+	} else if discL, err = gtk.LabelNew("Disc:"); err != nil {
+		w.log.Printf("[ERROR] Cannot create Disc Label: %s\n",
+			err.Error())
+		return
+	} else if titleE, err = gtk.EntryNew(); err != nil {
+		w.log.Printf("[ERROR] Cannot create Title Entry: %s\n",
+			err.Error())
+		return
+	} else if urlE, err = gtk.EntryNew(); err != nil {
+		w.log.Printf("[ERROR] Cannot create URL Entry: %s\n",
+			err.Error())
+		return
+	} else if trackE, err = gtk.SpinButtonNewWithRange(1, 2000, 1); err != nil {
+		w.log.Printf("[ERROR] Cannot create Track SpinButton: %s\n",
+			err.Error())
+		return
+	} else if discE, err = gtk.SpinButtonNewWithRange(1, 100, 1); err != nil {
+		w.log.Printf("[ERROR] Cannot create Disc SpinButton: %s\n",
+			err.Error())
+		return
+	} else if dbox, err = dlg.GetContentArea(); err != nil {
+		w.log.Printf("[ERROR] Cannot get ContentArea of EditFile Dialog: %s\n",
+			err.Error())
+		return
+	}
+
+	grid.InsertColumn(0)
+	grid.InsertColumn(1)
+
+	grid.InsertRow(0)
+	grid.InsertRow(1)
+	grid.InsertRow(2)
+	grid.InsertRow(3)
+
+	grid.Attach(titleL, 0, 0, 1, 1)
+	grid.Attach(titleE, 1, 0, 1, 1)
+	grid.Attach(urlL, 0, 1, 1, 1)
+	grid.Attach(urlE, 1, 1, 1, 1)
+	grid.Attach(discL, 0, 2, 1, 1)
+	grid.Attach(discE, 1, 2, 1, 1)
+	grid.Attach(trackL, 0, 3, 1, 1)
+	grid.Attach(trackE, 1, 3, 1, 1)
+
+	titleE.SetText(f.DisplayTitle())
+	urlE.SetText(f.URL)
+	discE.SetValue(float64(f.Ord[0]))
+	trackE.SetValue(float64(f.Ord[1]))
+
+	dbox.PackStart(grid, true, true, 0)
+	dlg.ShowAll()
+
+	var res = dlg.Run()
+
+	switch res {
+	case gtk.RESPONSE_NONE:
+		fallthrough
+	case gtk.RESPONSE_DELETE_EVENT:
+		fallthrough
+	case gtk.RESPONSE_CLOSE:
+		fallthrough
+	case gtk.RESPONSE_CANCEL:
+		w.log.Println("[DEBUG] User changed their mind about adding a Program. Fine with me.")
+		return
+	case gtk.RESPONSE_OK:
+		// 's ist los, Hund?
+	default:
+		w.log.Printf("[CANTHAPPEN] Well, I did NOT see this coming: %d\n",
+			res)
+		return
+	}
+
+	var (
+		title, uri  string
+		track, disc int
+		txstatus    bool
+		d           *db.Database
+	)
+
+	title, _ = titleE.GetText()
+	uri, _ = urlE.GetText()
+	disc = discE.GetValueAsInt()
+	track = trackE.GetValueAsInt()
+
+	d = w.pool.Get()
+	defer w.pool.Put(d)
+
+	if err = d.Begin(); err != nil {
+		w.log.Printf("[ERROR] Cannot start transaction: %s\n",
+			err.Error())
+		return
+	}
+
+	defer func() {
+		if txstatus {
+			d.Commit()
+		} else {
+			d.Rollback()
+		}
+	}()
+
+	if title != f.Title {
+		w.log.Printf("[DEBUG] Title: %q -> %q\n",
+			f.Title, title)
+		if err = d.FileSetTitle(f, title); err != nil {
+			w.log.Printf("[ERROR] Cannot set Title of File %d (%s) to %s: %s\n",
+				f.ID,
+				f.Title,
+				title,
+				err.Error())
+			return
+		}
+	}
+
+	if uri != f.URL {
+		w.log.Printf("[DEBUG] URL: %q -> %q\n",
+			f.URL, uri)
+		w.displayMsg("Updating File URL is not implemented yet")
+	}
+
+	if disc != int(f.Ord[0]) || track != int(f.Ord[1]) {
+		w.log.Printf("[DEBUG] Ord: %d/%d -> %d/%d\n",
+			f.Ord[0], f.Ord[1],
+			disc, track)
+		var ord = []int64{int64(disc), int64(track)}
+		if err = d.FileSetOrd(f, ord); err != nil {
+			w.log.Printf("[ERROR] Cannot set Disc/Track of File %d (%s): %s\n",
+				f.ID,
+				f.Title,
+				err.Error())
+			return
+		}
+	}
+
+	txstatus = true
 } // func (w *RWin) editFile(f *objects.File, iter *gtk.TreeIter)
 
 func (w *RWin) editProgram(p *objects.Program, iter *gtk.TreeIter) {
@@ -1323,7 +1478,7 @@ func (w *RWin) editProgram(p *objects.Program, iter *gtk.TreeIter) {
 			err.Error())
 		return
 	} else if dbox, err = dlg.GetContentArea(); err != nil {
-		w.log.Printf("[ERROR] Cannot get ContentArea of AddPerson Dialog: %s\n",
+		w.log.Printf("[ERROR] Cannot get ContentArea of EditProgram Dialog: %s\n",
 			err.Error())
 		return
 	}

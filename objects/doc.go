b1418adbd587149679e4b29d0dac0b23c957a2d7
Benjamin Walkenhorst <krylon@posteo.de>
2023-09-15 17:19:33 +0200

    Add package-level documentation to package objects

---
+++ objects/doc.go
@@ -0,0 +1,12 @@
+// /home/krylon/go/src/github.com/blicero/raconteur/objects/doc.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 15. 09. 2023 by Benjamin Walkenhorst
+// (c) 2023 Benjamin Walkenhorst
+// Time-stamp: <2023-09-15 12:32:15 krylon>
+
+// This package contains no code, it just exist so I have a place to put
+// the package documentation comment.
+
+// Package objects provides the major data types representing the objects
+// the application deals with - folders, programs, and files.
+package objects
